1966a0c78197ebce5727025934fc14e1b31ba44d
pawoods <>
2023-09-24 22:26:35 +0000

    adds DELETE to delete task

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -34,6 +34,7 @@ def edit_category(category_id):
         return redirect(url_for("categories"))
     return render_template("edit_category.html", category=category)
 
+
 @app.route("/delete_category/<int:category_id>")
 def delete_category(category_id):
     category = Category.query.get_or_404(category_id)
@@ -73,3 +74,12 @@ def edit_task(task_id):
         db.session.commit()
         return redirect(url_for("home"))
     return render_template("edit_task.html", categories=categories, task=task)
+
+
+@app.route("/delete_task/<int:task_id>")
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for("home"))
+
